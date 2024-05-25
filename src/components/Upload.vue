051faca1f753fae599dcf9372359a47358beea5a
gsxhnd <gsxhnd@gmail.com>
2024-05-25 19:31:38 +0800

    test

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -5,7 +5,6 @@
       mode="basic"
       name="demo[]"
       customUpload
-      @uploader="onTemplatedUpload"
       @select="onSelectedFiles"
     />
   </div>
@@ -14,34 +13,25 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import FileUpload, { FileUploadSelectEvent } from "primevue/fileupload";
-import Button from "primevue/button";
+// import Button from "primevue/button";
 // import { useToast } from "primevue/usetoast";
 
 // const toast = useToast();
 
-const totalSize = ref(0);
-const totalSizePercent = ref(0);
+// const totalSize = ref(0);
+// const totalSizePercent = ref(0);
 const files = ref([]);
-
-const onTemplatedUpload = (evnet: any) => {
-  // toast.add({
-  //   severity: "info",
-  //   summary: "Success",
-  //   detail: "File Uploaded",
-  //   life: 3000,
-  // });
-};
 
 const onSelectedFiles = (event: FileUploadSelectEvent) => {
   console.log(event);
   files.value = event.files;
-  files.value.forEach((file) => {
-    // totalSize.value += parseInt(formatSize(file.size));
-  });
+  // files.value.forEach((file) => {
+  //   // totalSize.value += parseInt(formatSize(file.size));
+  // });
   let path: string = event.files[0].path;
   window.electronAPI.copy({
     from: path,
-    to: "/home/gsxhnd/Code/personal/Ourboros/test.json",
+    to: "",
   });
 };
 </script>
