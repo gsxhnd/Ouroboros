aadc8fe5155a043f1068d06eb9ef2e07c93c8617
gsxhnd <gsxhnd@gmail.com>
2024-07-30 16:14:30 +0800

    feat: add api

--- ouroboros-api/src/main.rs
+++ ouroboros-api/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello world")
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    println!("Hello world");
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/", post(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
