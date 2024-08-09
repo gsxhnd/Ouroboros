b10496984bd015a57a729703b4bf289d4858f0e4
gsxhnd <gsxhnd@gmail.com>
2024-08-09 16:53:15 +0800

    update

--- ouroboros-api/src/handler/file.rs
+++ ouroboros-api/src/handler/file.rs
@@ -1,9 +1,25 @@
 use crate::state::AppState;
 
-use axum::{extract::State, response::IntoResponse, Json};
+use axum::{
+    extract::{Query, RawQuery, State},
+    response::IntoResponse,
+    Json,
+};
+use std::collections::HashMap;
 
-pub async fn get_files(state: State<AppState>) -> impl IntoResponse {
-    let data = match state.conn.get_files_by_folder_id(6).await {
+pub async fn get_files(
+    state: State<AppState>,
+    Query(params): Query<HashMap<String, String>>,
+) -> impl IntoResponse {
+    let folder_id: u32 = match params.get("folder_id") {
+        Some(folder_id) => match folder_id.parse() {
+            Ok(v) => v,
+            Err(_e) => 0,
+        },
+        None => 0,
+    };
+
+    let data = match state.conn.get_files_by_folder_id(folder_id).await {
         Ok(data) => match data {
             Some(data) => Some(data),
             None => None,
