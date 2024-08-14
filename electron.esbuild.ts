c0c128c7da0f0c9b077b7d6a100d2f000fab26b3
gsxhnd <gsxhnd@gmail.com>
2024-08-14 20:35:50 +0800

    update

--- electron.esbuild.ts
+++ electron.esbuild.ts
@@ -2,54 +2,37 @@ import { build, context, BuildOptions } from "esbuild";
 // import { wasmPlugin } from "./wasm.plugin.js";
 // import watPlugin from "esbuild-plugin-wat";
 
-let entryPoints = [
-  "./ouroboros-electron/main.ts",
-  "./ouroboros-electron/preload.ts",
-];
-let external = ["path", "electron", "fs", "chokidar", "rxjs"];
+let buildOptions: BuildOptions = {
+  entryPoints: [
+    "./ouroboros-electron/main.ts",
+    "./ouroboros-electron/preload.ts",
+  ],
+  bundle: true,
+  platform: "node",
+  format: "cjs",
+  external: ["path", "electron", "fs", "chokidar", "rxjs"],
+  outdir: "./dist/",
+  minify: process.env.NODE_ENV === "prod",
+  outExtension: {
+    ".js": ".cjs",
+  },
+  loader: {
+    ".node": "copy",
+    ".wasm": "copy",
+  },
+};
 
-// let buildOptions: BuildOptions = {
-//   entry
-// };
-
-async function devWatch() {
-  let ctx = await context({
-    entryPoints: entryPoints,
-    bundle: true,
-    platform: "node",
-    format: "cjs",
-    external: external,
-    outdir: "./dist/",
-    minify: false,
-    outExtension: {
-      ".js": ".cjs",
-    },
-    loader: {
-      ".node": "copy",
-      ".wasm": "copy",
-    },
-  });
-
+const devWatch = async function () {
+  let ctx = await context(buildOptions);
   await ctx.watch();
-}
+};
 
-async function prodBuild() {
-  build({
-    entryPoints: entryPoints,
-    bundle: true,
-    platform: "node",
-    format: "cjs",
-    external: external,
-    outdir: "./dist/",
-    minify: true,
-    outExtension: {
-      ".js": ".cjs",
-    },
-    loader: {
-      ".node": "copy",
-      ".wasm": "copy",
-    },
-  })
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "dev") {
+  devWatch();
+  console.log("watch");
+} else {
+  build(buildOptions)
     .then(() => {
       console.log("electron build success");
     })
@@ -57,12 +40,4 @@ async function prodBuild() {
       console.error("electron build failed");
       console.error(err);
     });
-}
-
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === "dev") {
-  devWatch();
-  console.log("watch");
-} else {
-  prodBuild();
 }
