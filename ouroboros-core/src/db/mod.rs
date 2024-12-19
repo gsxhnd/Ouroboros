88c5ad647b818ab3225ade7389d7d58b32b91b2f
gsxhnd <gsxhnd@gmail.com>
2024-12-19 09:16:53 +0800

    try new database

--- ouroboros-core/src/db/mod.rs
+++ ouroboros-core/src/db/mod.rs
@@ -4,6 +4,12 @@ use sqlx::{
     ConnectOptions, Pool, Sqlite,
 };
 
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::local::RocksDb;
+use surrealdb::RecordId;
+use surrealdb::Surreal;
+// use surrealdb::engine::local
+
 mod file;
 mod file_tag;
 mod folder;
@@ -12,6 +18,19 @@ mod tag;
 #[derive(Debug, Clone)]
 pub struct Database {
     pool: Pool<Sqlite>,
+}
+
+#[derive(Debug, Serialize)]
+struct Person {
+    title: String,
+    name: String,
+    marketing: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    #[allow(dead_code)]
+    id: RecordId,
 }
 
 impl Database {
@@ -29,6 +48,20 @@ impl Database {
             .await
             .unwrap();
 
+        let db = Surreal::new::<RocksDb>("./data/123").await.unwrap();
+        db.use_ns("test").use_db("test").await.unwrap();
+
+        let created: Option<Record> = db
+            .create("person")
+            .content(Person {
+                title: "Founder & CEO".to_string(),
+                name: "Founder & CEO".to_string(),
+                marketing: true,
+            })
+            .await
+            .unwrap();
+        let people: Vec<Record> = db.select("person").await.unwrap();
+        dbg!(people);
         Database { pool }
     }
 }
